10a65795e42f071e83337750ca2d8ccda3879ca1
Arnaud Babol <arnaud@babol.me>
2015-08-31 02:08:47 +0200

    fix pure

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -20,7 +20,7 @@ setopt NO_BG_NICE # don't nice background tasks
 setopt NO_HUP
 setopt NO_LIST_BEEP
 setopt LOCAL_OPTIONS # allow functions to have local options
-setopt LOCAL_TRAPS # allow functions to have local traps
+# setopt LOCAL_TRAPS # allow functions to have local traps - BROKE PURE GIT DIRTY
 setopt HIST_VERIFY
 setopt SHARE_HISTORY # share history between sessions ???
 setopt EXTENDED_HISTORY # add timestamps to history
